95149e065c3a3b9c475816c6995955a6621c9c7b
Lyn <lynfinman@gmail.com>
2022-07-07 13:03:53 -0400

    commit tree 51e11a79d7a9f2c27efc43c62584408d60f43620

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  if array.size == 1
+    array[0]
 
+  elsif array.size == 2
+    array.join(' and ')
+  else
+    last_element = array.pop
+    comma_string = array.join(', ')
+    comma_string << ", and #{last_element}"
+  end
 end
+
+# puts oxford_comma(%w[fiddleheads okra kohlrabi])
